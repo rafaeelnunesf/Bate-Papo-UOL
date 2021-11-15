5223ef0a6ca43b01c7c8b24c887ae4de13455dc7
rafaeelnunes <rafaelnfsq@gmail.com>
2021-11-15 19:53:09 -0300

    adicionando alguns comentários (revisão)

--- src/scripts.js
+++ src/scripts.js
@@ -5,8 +5,9 @@ let typeMessage = 'message'
 function wait() {
     const divWait = document.querySelector('.wait')
     const inputs = document.querySelector('.inputs')
-
+    // Esconder o botao e o input no inicio
     inputs.classList.add('hidden')
+    // Mostrar o icone de carregamento
     divWait.classList.remove('hidden')
     setTimeout(userEnter,1000)
 }
@@ -14,15 +15,14 @@ function wait() {
 function userEnter() {
     const inputScreen = document.querySelector('.input-screen')
     userName = document.querySelector('.input-screen input').value
+
+    // Caso o usuário não insira nenhum nome
     if(userName===''){
         alert('insira um nome por favor')
         window.location.reload()
         return
     }
-
-    if(inputScreen.classList.contains('hidden')){
-        inputScreen.classList.remove('hidden')
-    }
+    // Esconde a tela de inicio
     inputScreen.classList.add('hidden')
     EnterRoom()
 }
@@ -31,13 +31,16 @@ let EnterRoom = () => {
     userName = {
         name: userName
     }
-
+    // usuario entra na sala
     let registerUser = axios.post('https://mock-api.driven.com.br/api/v4/uol/participants',userName)
+
+    // Se der erro é porque o usuário já está cadastrado
     registerUser.catch(errorRegister)
     function errorRegister(answer) {
         alert('usuário já cadastrado, tente outro nome por favor')
         window.location.reload()
     }
+
     GetMessages()
     setInterval(GetMessages,3000)
     setInterval(keepConection,5000,userName)
@@ -95,9 +98,12 @@ function GetMessages(){
             }
         }
         
+        // Escreve  as mensagens no HTML 
         for (let i =0; i< array.length;i++){
             messagesHTML.innerHTML += array[i]
         }
+
+        // 'Scrolla' até a ultima mensagem 
         messagesHTML.children[array.length-1].scrollIntoView();
     }
 }
@@ -113,9 +119,11 @@ let sendMessage = () =>{
     }
     text.value = ''
 
+    //Se não estiver nada escrito no campo de mensagens não acontece nada
     if (message.text===""){
         return
     }
+
     let request = axios.post('https://mock-api.driven.com.br/api/v4/uol/messages',message)
 
     request.then(successSendMsg)
@@ -133,18 +141,25 @@ let sendMessage = () =>{
 function addTransition(){
     getParticipants()
     setInterval(getParticipants,10000)
-
-    const public = document.querySelector('.public').children[2]
-    const private = document.querySelector('.private').children[2]
+    
+    // Mudar pra undefined pra não aparecer marcado quando abrir o menu de participantes
     toUser = undefined
     typeMessage = undefined
+    // Adicionar a classe hidden pra não aparecer marcado quando abrir o menu de participantes
+    const public = document.querySelector('.public').children[2]
+    const private = document.querySelector('.private').children[2]
     public.classList.add('hidden')
     private.classList.add('hidden')
 
+    // Adiciona a classe index para o menu de participantes sobrepor as mensagens, header e footer
     let header = document.querySelector('header')
     let footer = document.querySelector('.footer')
-    let messages = document.querySelector('.messages')
-    
+    let messages = document.querySelector('.messages') 
+    header.classList.add('add-index')
+    footer.classList.add('add-index')
+    messages.classList.add('add-index-messages')
+
+    // Tira a clase Hidden e adiciona classe transition
     let activeParticipants = document.querySelector('.active-participants')
     let background = document.querySelector('.background')
     let contacts = document.querySelector('.contacts')
@@ -157,9 +172,6 @@ function addTransition(){
     background.classList.add('transition')
     contacts.classList.add('transition')
 
-    header.classList.add('add-index')
-    footer.classList.add('add-index')
-    messages.classList.add('add-index-messages')  
 }
 
 function getParticipants() {
@@ -167,23 +179,24 @@ function getParticipants() {
     promiseGetParticipants.then(successGetParticipants)
     function successGetParticipants(answer) {
         let users = document.querySelector('.users')
+        let participants = answer.data
+
         users.innerHTML = ""
         users.innerHTML = `
-        <div class="everyone" onclick="chooseContact(this)">
-            <ion-icon name="people" class="profile-icon"></ion-icon>
-            <p>Todos</p>
-            <ion-icon name="checkmark-sharp" class="check hidden"></ion-icon>
-        </div>
-        `
-        let participants = answer.data
-        for(let i =0; i<participants.length;i++){
-            users.innerHTML += `
-            <div class="participant" data-identifier="participant" onclick="chooseContact(this)">
-                <ion-icon name="person-circle" class="profile-icon"></ion-icon>
-                <p>${participants[i].name}</p>
+            <div class="everyone" onclick="chooseContact(this)">
+                <ion-icon name="people" class="profile-icon"></ion-icon>
+                <p>Todos</p>
                 <ion-icon name="checkmark-sharp" class="check hidden"></ion-icon>
             </div>
             `
+        for(let i =0; i<participants.length;i++){
+            users.innerHTML += `
+                <div class="participant" data-identifier="participant" onclick="chooseContact(this)">
+                    <ion-icon name="person-circle" class="profile-icon"></ion-icon>
+                    <p>${participants[i].name}</p>
+                    <ion-icon name="checkmark-sharp" class="check hidden"></ion-icon>
+                </div>
+                `
         }
     }   
 }
@@ -192,6 +205,7 @@ function chooseContact(contact) {
     const check = contact.children[2]
     const div = contact.parentNode
 
+    // Caso o usuário selecione Todos, automaticamente seleciona a visibilidade publico
     if(contact===div.children[0]){
         const visibility = document.querySelector('.public')
         check.classList.remove('hidden')
@@ -201,6 +215,8 @@ function chooseContact(contact) {
         return
     }
 
+    // Para o caso de ser o 2 clique no menu de participante
+    // Verifica todos os particiopantes e tira o check se existir algum marcado
     for(let i =0; i<div.childElementCount;i++){
         let iconCheck = div.children[i].children[2]
         let condition = iconCheck.classList.contains('hidden')
@@ -217,6 +233,8 @@ function chooseVisibility(visibility) {
     const check = visibility.children[2]
     const div = visibility.parentNode
 
+    // Para o caso de ser o 2 clique no menu de participante
+    // Verifica todos os visibilidades e tira o check se existir alguma marcada
     for(let i =0; i<div.childElementCount;i++){
         let iconCheck = div.children[i].children[2]
         let condition = iconCheck.classList.contains('hidden')
@@ -234,7 +252,8 @@ function chooseVisibility(visibility) {
     removeTransition()
 }
 function removeTransition(background) {
-    privateMsg()
+    infoMsg()
+
     if(background !== undefined){
         toUser = 'Todos'
         typeMessage = 'message'
@@ -264,7 +283,7 @@ function removeTransition(background) {
     }
 }
 
-function privateMsg(){
+function infoMsg(){
     const textBoxInfo = document.querySelector('.message-info')
     if(typeMessage==='message'){
         textBoxInfo.innerHTML = `Enviando para&nbsp<span class="to-message">${toUser}</span>&nbsp(Público)`
@@ -279,7 +298,8 @@ document.onkeyup=function(e){
     return
     } 
     const element = document.querySelector('.input-screen.hidden')
-    if(!!element){
+    
+    if(!!element){//se for true
         sendMessage()
     }else{
         wait()
