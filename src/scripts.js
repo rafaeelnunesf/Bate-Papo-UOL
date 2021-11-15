fb046adec8bab55c8fbad10a350f7f6fcdfa4cdb
rafaeelnunes <rafaelnfsq@gmail.com>
2021-11-15 19:02:34 -0300

    projeto finalizado

--- src/scripts.js
+++ src/scripts.js
@@ -1,53 +1,32 @@
 let userName
 let toUser = 'Todos'
 let typeMessage = 'message'
+
+function wait() {
+    const divWait = document.querySelector('.wait')
+    const inputs = document.querySelector('.inputs')
+
+    inputs.classList.add('hidden')
+    divWait.classList.remove('hidden')
+    setTimeout(userEnter,1000)
+}
+
 function userEnter() {
     const inputScreen = document.querySelector('.input-screen')
+    userName = document.querySelector('.input-screen input').value
+    if(userName===''){
+        alert('insira um nome por favor')
+        window.location.reload()
+        return
+    }
+
     if(inputScreen.classList.contains('hidden')){
         inputScreen.classList.remove('hidden')
     }
-    userName = document.querySelector('.input-screen input').value
     inputScreen.classList.add('hidden')
     EnterRoom()
 }
-function addTransition(){
-    getParticipants()
-    setInterval(getParticipants,10000)
 
-    const textBoxInfo = document.querySelector('.message-info')
-    const condition = textBoxInfo.classList.contains('display')
-    if (!condition) {
-        textBoxInfo.classList.add('display')
-    }
-
-    const public = document.querySelector('.public').children[2]
-    const private = document.querySelector('.private').children[2]
-    toUser = undefined
-    typeMessage = undefined
-    public.classList.add('hidden')
-    private.classList.add('hidden')
-
-    let header = document.querySelector('header')
-    let footer = document.querySelector('.footer')
-    let messages = document.querySelector('.messages')
-    
-    let activeParticipants = document.querySelector('.active-participants')
-    let background = document.querySelector('.background')
-    let contacts = document.querySelector('.contacts')
-    
-    activeParticipants.classList.remove('hidden')
-    background.classList.remove('hidden')
-    contacts.classList.remove('hidden')
-
-    activeParticipants.classList.add('transition')
-    background.classList.add('transition')
-    contacts.classList.add('transition')
-
-    header.classList.add('add-index')
-    footer.classList.add('add-index')
-    messages.classList.add('add-index-messages')  
-  }
-// Essa função entra na sala
 let EnterRoom = () => {
     userName = {
         name: userName
@@ -64,13 +43,10 @@ let EnterRoom = () => {
     setInterval(keepConection,5000,userName)
 }
 
-
-//Essa função mantém o usuário conectado
 function keepConection(Name) {
     axios.post('https://mock-api.driven.com.br/api/v4/uol/status',Name) 
 }
 
-// Essa função carrega as mensagens
 function GetMessages(){
     let promiseGetMessages = axios.get('https://mock-api.driven.com.br/api/v4/uol/messages')
     promiseGetMessages.then(LoadMessages)
@@ -129,10 +105,6 @@ function GetMessages(){
 let sendMessage = () =>{
     const text = document.querySelector('.footer input')
 
-    console.log(userName.name);
-    console.log(toUser);
-    console.log(text.value);
-    console.log(typeMessage);
     let message = {
         from: userName.name,
         to: toUser,
@@ -140,6 +112,10 @@ let sendMessage = () =>{
         type: typeMessage
     }
     text.value = ''
+
+    if (message.text===""){
+        return
+    }
     let request = axios.post('https://mock-api.driven.com.br/api/v4/uol/messages',message)
 
     request.then(successSendMsg)
@@ -149,11 +125,41 @@ let sendMessage = () =>{
         GetMessages() 
     }
     function errorSendMsg(answer) {
-        console.log(answer.response)
         alert('usuário saiu da sala')
         window.location.reload()  
     }
+}
 
+function addTransition(){
+    getParticipants()
+    setInterval(getParticipants,10000)
+
+    const public = document.querySelector('.public').children[2]
+    const private = document.querySelector('.private').children[2]
+    toUser = undefined
+    typeMessage = undefined
+    public.classList.add('hidden')
+    private.classList.add('hidden')
+
+    let header = document.querySelector('header')
+    let footer = document.querySelector('.footer')
+    let messages = document.querySelector('.messages')
+    
+    let activeParticipants = document.querySelector('.active-participants')
+    let background = document.querySelector('.background')
+    let contacts = document.querySelector('.contacts')
+    
+    activeParticipants.classList.remove('hidden')
+    background.classList.remove('hidden')
+    contacts.classList.remove('hidden')
+
+    activeParticipants.classList.add('transition')
+    background.classList.add('transition')
+    contacts.classList.add('transition')
+
+    header.classList.add('add-index')
+    footer.classList.add('add-index')
+    messages.classList.add('add-index-messages')  
 }
 
 function getParticipants() {
@@ -172,7 +178,7 @@ function getParticipants() {
         let participants = answer.data
         for(let i =0; i<participants.length;i++){
             users.innerHTML += `
-            <div class="participant" onclick="chooseContact(this)">
+            <div class="participant" data-identifier="participant" onclick="chooseContact(this)">
                 <ion-icon name="person-circle" class="profile-icon"></ion-icon>
                 <p>${participants[i].name}</p>
                 <ion-icon name="checkmark-sharp" class="check hidden"></ion-icon>
@@ -191,7 +197,7 @@ function chooseContact(contact) {
         check.classList.remove('hidden')
         toUser = contact.children[1].innerHTML
         chooseVisibility(visibility)
-        removetransition()
+        removeTransition()
         return
     }
 
@@ -205,7 +211,7 @@ function chooseContact(contact) {
     check.classList.remove('hidden')
     toUser = contact.children[1].innerHTML
     
-    removetransition()
+    removeTransition()
 }
 function chooseVisibility(visibility) {
     const check = visibility.children[2]
@@ -225,18 +231,17 @@ function chooseVisibility(visibility) {
     }else if(visibility===div.children[1]){
         typeMessage = 'private_message'
     }
-    removetransition()
+    removeTransition()
 }
-function removetransition(background) {
-    console.log(background);
-
+function removeTransition(background) {
+    privateMsg()
     if(background !== undefined){
         toUser = 'Todos'
         typeMessage = 'message'
     }
 
     if(toUser!==undefined && typeMessage!==undefined){
-        privateMsg()
+
         let header = document.querySelector('header')
         let footer = document.querySelector('.footer')
         let messages = document.querySelector('.messages')
@@ -258,12 +263,25 @@ function removetransition(background) {
         messages.classList.remove('add-index-messags')
     }
 }
+
 function privateMsg(){
-    if (toUser === 'Todos'|| typeMessage ==='message'){
-        return
-    }
     const textBoxInfo = document.querySelector('.message-info')
-    console.log(textBoxInfo);
-    textBoxInfo.classList.remove('display')
-    textBoxInfo.innerHTML = `Enviando para&nbsp<p> ${toUser}</p>(reservadamente)`
+    if(typeMessage==='message'){
+        textBoxInfo.innerHTML = `Enviando para&nbsp<span class="to-message">${toUser}</span>&nbsp(Público)`
+    }else if(typeMessage==='private_message'){
+        textBoxInfo.innerHTML = `Enviando para&nbsp<span class="to-message">${toUser}</span>(Reservadamente)`
+    }
+}
+
+// Entrar na sala e envio com enter
+document.onkeyup=function(e){
+    if(e.which !== 13){
+    return
+    } 
+    const element = document.querySelector('.input-screen.hidden')
+    if(!!element){
+        sendMessage()
+    }else{
+        wait()
+    }
 }
